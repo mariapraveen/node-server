3594c6dbf5627f01ee457d4d4fc7dbccef6bac23
mariaantony.gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2020-05-30 18:58:08 +0530

    initial login commit

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const { createServer } = require('http');
+const { parse } = require('url');
 
-const server = createServer((req, res) => {
-    res.write('hello');
-    res.end();
+const { enableCors, getRequestData  } = require('./lib/helper');
+const { createUser, login } = require('./lib/user');
+
+const server = createServer(async (req, res) => {
+    enableCors(res);
+    if (req.method.toLowerCase() === 'options') {
+        res.writeHead(204);
+        res.end();
+    } else {
+        await processMethod(req, res);
+    }
 });
 
 server.listen(9000);
+
+async function processMethod(req, res) {
+    let data = await getRequestData(req);
+    let parsedUrl = parse(req.url);
+    switch (`${req.method.toLowerCase() + parsedUrl.pathname.toLowerCase()}`) {
+        case 'put/create':
+            res.end(createUser(JSON.parse(data)));
+            break;
+        case 'post/login':
+            res.end(login(JSON.parse(data)));
+            break;
+        default:
+            res.end();
+    }
+}
+
+
